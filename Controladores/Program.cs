64aa8ef73b41372d34f254b399338587b9e439b2
Jesús <csi22-jalvugo@colegioaltair.net>
2023-09-27 09:33:09 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,27 @@
+﻿using CajeroMenuC.Servicios;
+
+namespace CajeroMenuC.Controladores
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// 250923 - jal
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método de entrada de la aplicación
+        /// 250923 - jal
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+
+            MenuInterfaz mi = new MenuImplementacion();
+
+            mi.mostrarMensajeBienvenida();
+
+        }
+    } 
+}
+            
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroMenuC.Servicios
+{
+    /// <summary>
+    /// Interfaz con la relación de métodos de la funcionalidad menú
+    /// 250923 - jal
+    /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Método que muestra mensaje de bienvenida al cajero.
+        /// </summary>
+        void mostrarMensajeBienvenida();
+
+        public int mostrarMenuYSeleccion();
+    }
+
+    
+}
